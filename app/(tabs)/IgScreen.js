4dbcbfcf0d0917b0509510cb77af3eda9c818fc4
Bao44 <tqbao44@gmail.com>
2024-11-11 15:12:41 +0700

    fix screen ig

--- app/(tabs)/IgScreen.js
+++ app/(tabs)/IgScreen.js
@@ -1,15 +1,4 @@
-import {
-  Alert,
-  Button,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import { useAuth } from "../../contexts/AuthContext";
@@ -18,127 +7,10 @@ import { hp, wp } from "../../helpers/common";
 import Icon from "../../assets/icons";
 import { useRouter } from "expo-router";
 import Avatar from "../../components/Avatar";
-import ig from "../../assets/data/ig.json";
-import Entypo from "@expo/vector-icons/Entypo";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import Feather from "@expo/vector-icons/Feather";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import ModalComment from "../../utils/ModalComment";
-import ModalShare from "../../utils/ModalShare";
 
 const LinkUpScreen = () => {
   const { user, setAuth } = useAuth();
   const router = useRouter();
-
-  const [modalComment, setModalComment] = useState(false);
-  const [modalShare, setModalShare] = useState(false);
-  const [selectedHeart, setSelectedHeart] = useState({});
-  const [showDescription, setShowDescription] = useState({});
-
-  const toggleHeart = (id) => {
-    setSelectedHeart((prevState) => ({
-      ...prevState,
-      [id]: !prevState[id],
-    }));
-  };
-
-  const toggleDescription = (id) => {
-    setShowDescription((prevState) => ({
-      ...prevState,
-      [id]: !prevState[id],
-    }));
-  };
-
-  const renderStory = ({ item }) => {
-    return (
-      <View>
-        <TouchableOpacity>
-          <Image source={{ uri: item.avatar }} style={styles.avatarStory} />
-        </TouchableOpacity>
-        <Text style={styles.nameStory}>{item.name}</Text>
-      </View>
-    );
-  };
-
-  const renderInformation = ({ item }) => {
-    return (
-      <View style={{ marginHorizontal: 10 }}>
-        <View style={styles.headerInformation}>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Image
-              source={{ uri: item.avatar }}
-              style={styles.avatarInformation}
-            />
-            <Text style={styles.nameInformation}>{item.name}</Text>
-          </View>
-          <Entypo name="dots-three-vertical" size={22} color="black" />
-        </View>
-        <Image source={{ uri: item.image }} style={styles.imageInformation} />
-        <View style={styles.viewIconEvent}>
-          <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity onPress={() => toggleHeart(item.id)}>
-              <AntDesign
-                name={selectedHeart[item.id] ? "heart" : "hearto"}
-                size={30}
-                color={selectedHeart[item.id] ? "red" : "black"} // chỉ thay đổi cho item được click
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                setModalComment(!modalComment);
-              }}
-            >
-              <Feather
-                name="message-circle"
-                size={30}
-                color="black"
-                style={{ marginHorizontal: 20 }}
-              />
-            </TouchableOpacity>
-
-            <ModalComment
-              modalComment={modalComment}
-              setModalComment={setModalComment}
-              user = {user}
-            />
-
-            <TouchableOpacity
-              onPress={() => {
-                setModalShare(!modalShare);
-              }}
-            >
-              <FontAwesome5 name="share" size={30} color="black" />
-            </TouchableOpacity>
-
-            <ModalShare
-              modalShare={modalShare}
-              setModalShare={setModalShare}
-            />
-          </View>
-          <TouchableOpacity>
-            <FontAwesome name="bookmark-o" size={30} color="black" />
-          </TouchableOpacity>
-        </View>
-        <Text style={styles.nameContent}>
-          {item.name} <Text style={styles.content}>CONTENT</Text>
-        </Text>
-        <Text
-          style={styles.description}
-          numberOfLines={showDescription[item.id] ? undefined : 1} // Hiển thị tất cả các dòng nếu showDes là true
-          ellipsizeMode="tail" // Hiển thị dấu "..." nếu nội dung bị cắt
-        >
-          Description anh bảo đẹp trai số 1 thế giới siêu cấp đẹp trai không ai
-          sánh bằng hahaha
-        </Text>
-        <TouchableOpacity onPress={() => toggleDescription(item.id)}>
-          <Text style={styles.showDescription}>
-            {showDescription[item.id] ? "Ẩn bớt" : "Xem thêm"}
-          </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
 
   return (
     <ScreenWrapper bg="white">
@@ -176,16 +48,6 @@ const LinkUpScreen = () => {
             </View>
           </View>
         </View>
-        {/*  View Screen Home */}
-        {/* Story */}
-        <FlatList data={ig} renderItem={renderStory} horizontal />
-        {/* Information */}
-        <FlatList
-          data={ig}
-          renderItem={renderInformation}
-          scrollEnabled={false}
-        />
-        <View style={{ marginBottom: 30 }}></View>
       </ScrollView>
     </ScreenWrapper>
   );
